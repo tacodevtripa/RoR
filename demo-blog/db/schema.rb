4fbe6b37d9b42452e03175598fe3698ccf839aa4
tacodevtripa <rfigueroa.fullstackdev@gmail.com>
2025-01-23 16:13:10 -0600

    create Posts table

--- demo-blog/db/schema.rb
+++ demo-blog/db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_23_174654) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_23_213436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.string "title", null: false
+    t.text "text", null: false
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +32,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_23_174654) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "posts", "users", column: "author_id"
 end
