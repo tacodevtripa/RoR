a92885e1c4c92bbaad0ab7db73101b401b109fa2
tacodevtripa <rfigueroa.fullstackdev@gmail.com>
2025-01-24 10:21:16 -0600

    set user custom method for recent posts

--- demo-blog/app/models/user.rb
+++ demo-blog/app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
-    has_many :posts
+    has_many :posts, foreign_key: "author_id"
     has_many :comments
     has_many :likes
+
+    def recent_posts
+        all_posts = self.posts.order(created_at: :desc)
+        recent_three = all_posts.take(3)
+        [ recent_three ]
+    end
+
+    private
 end
