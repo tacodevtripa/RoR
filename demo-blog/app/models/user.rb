563871162a265331fa5511570cd276487ced7b56
tacodevtripa <rfigueroa.fullstackdev@gmail.com>
2025-01-24 14:57:23 -0600

    set user data validations

--- demo-blog/app/models/user.rb
+++ demo-blog/app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
     has_many :comments
     has_many :likes
 
+    validates :name, presence: true
+    validates :posts_counter, numericality: {
+        only_integer: true,
+        greater_than_or_equal_to: 0
+    }
+
     def recent_posts
         all_posts = self.posts.order(created_at: :desc)
         recent_three = all_posts.take(3)
@@ -16,5 +22,6 @@ class User < ApplicationRecord
     def get_posts_counter
         self.posts.count
     end
+
     private
 end
