e375b01a53e095cefb31aa4b4dc5f402559d6b12
tacodevtripa <rfigueroa.fullstackdev@gmail.com>
2025-01-24 18:55:16 -0600

    create like and comment models unit tests

--- demo-blog/test/models/comment_test.rb
+++ demo-blog/test/models/comment_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create!(
+      name: "Test User",
+      posts_counter: 0
+    )
+    @post = Post.create!(
+      author: @user,
+      title: "Test Post",
+      text: "Test Text",
+      comments_counter: 0,
+      likes_counter: 0
+    )
+    @comment = Comment.new(
+      user: @user,
+      post: @post,
+      text: "test comment")
+  end
+  test "should belong to an user" do
+    assert_respond_to @comment, :user
+  end
+
+  test "should belong to a post" do
+    assert_respond_to @comment, :post
+  end
 end
--- demo-blog/test/models/like_test.rb
+++ demo-blog/test/models/like_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 class LikeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create!(
+      name: "Test User",
+      posts_counter: 0
+    )
+    @post = Post.create!(
+      author: @user,
+      title: "Test Post",
+      text: "Test Text",
+      comments_counter: 0,
+      likes_counter: 0
+    )
+    @like = Like.new(
+      user: @user,
+      post: @post)
+  end
+  test "should belong to an user" do
+    assert_respond_to @like, :user
+  end
+
+  test "should belong to a post" do
+    assert_respond_to @like, :post
+  end
 end
