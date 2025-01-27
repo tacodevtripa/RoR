c0a93431ad52477d4a405648122ad9ef3c242c02
tacodevtripa <rfigueroa.fullstackdev@gmail.com>
2025-01-27 13:38:07 -0600

    set up bootstrap

---
+++ demo-blog/app/assets/config/manifest.js
@@ -0,0 +1,7 @@
+// app / assets / config / manifest.js;
+
+//= link application.css
+//= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
